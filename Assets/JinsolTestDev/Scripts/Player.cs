05edc9ff075dad15d42c0660512dcec2ea2e9f0f
marrshmallow <152283030+marrshmallow@users.noreply.github.com>
2024-03-13 11:47:27 +0900

    도입부 연출

--- Assets/JinsolTestDev/Scripts/Player.cs
+++ Assets/JinsolTestDev/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Player : MonoBehaviour
 {
@@ -7,6 +8,10 @@ public class Player : MonoBehaviour
     private float sceneWidth;
     private Quaternion startRotation;
     //public float lookRotationDamper = 10f; 조금만 더 부드럽게 돌려주는 방법 찾으려다 포기한 부분
+    #endregion
+
+    #region 이벤트 컷씬 연출용 참조 영역
+    [SerializeField] private PlayableDirector director; // 상호작용으로 컷씬 불러오기 위해 인스펙터창에서 디렉터 참조
     #endregion
 
     private void Awake()
@@ -26,6 +31,21 @@ public class Player : MonoBehaviour
             float CurrentDistanceBetweenMousePositions = (Input.mousePosition - pressPoint).x;
             transform.rotation = startRotation * Quaternion.Euler((CurrentDistanceBetweenMousePositions / sceneWidth) * 180 * Vector3.up);
             //transform.rotation = startRotation * Quaternion.Slerp(CurrentDistanceBetweenMousePositions / sceneWidth * 180 * Vector3.up, lookRotationDamper);
+        }
+        #endregion
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        #region 이벤트 컷씬 발생
+        if (other.gameObject.tag == "Event")
+            director.Play();
+        #endregion
+
+        #region 회장 밖으로 나갔을 때 발생
+        if (other.gameObject.tag == "GetOut")
+        {
+            // UI 창을 띄워서 나갈 것인지 질문
         }
         #endregion
     }
