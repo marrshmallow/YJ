18745854437aaedfb0b48482b65259ac8217884a
Jinsol Jung <jinsolfernanda@gmail.com>
2024-03-22 16:42:10 +0900

    Dialogue System Edit

--- Assets/JinsolTestDev/Scripts/Dialogue/DialogueInteract.cs
+++ Assets/JinsolTestDev/Scripts/Dialogue/DialogueInteract.cs
@@ -1,20 +1,81 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueInteract : MonoBehaviour
 {
-    [SerializeField] DialogueSO dialogueSO;
+    #region 대화 표시 기능
+    [SerializeField] DialogueSO startDialogueObject;
+    [SerializeField] TextMeshProUGUI dialogueText;
+    private bool optionSelected = false; // 선택을 아직 안했으면 true
+    #endregion
+
+    #region UI 생성
+    [SerializeField] GameObject interactUI;
+    [SerializeField] GameObject textBubble;
+    [SerializeField] GameObject dialogueOptionsContainer;
+    [SerializeField] Transform dialogueOptionsParent;
+    [SerializeField] GameObject dialogueOptionsButtonPrefab;
+    #endregion
+
     public void StartDialogue()
     {
-        StartCoroutine(DisplayDialogue());
+        StartCoroutine(DisplayDialogue(startDialogueObject));
     }
 
-    private IEnumerator DisplayDialogue()
+    public void StartDialogue(DialogueSO dialogueObject)
     {
-        for (int i = 0; i < dialogueSO.dialogueLines.Count; i++ )
+        StartCoroutine(DisplayDialogue(dialogueObject));
+    }
+
+    public void OptionSelected(DialogueSO selectedOption)
+    {
+        optionSelected = true;
+        StartDialogue (selectedOption);
+    }
+
+    private IEnumerator DisplayDialogue(DialogueSO dialogueObject)
+    {
+        yield return null;
+        Debug.Log("Starting Dialogue Chain");
+        List<GameObject> instantiatedButtons = new List<GameObject>();
+
+        interactUI.SetActive(true);
+        Debug.Log("Dialogue Active");
+        foreach (var dialogue in startDialogueObject.dialogueSegments)
         {
-            Debug.Log(dialogueSO.dialogueLines[i]);
-            yield return new WaitForSeconds(1f);
+            dialogueText.text = dialogue.dialogueText;
+            
+            if (dialogue.dialogueChoices.Count == 0)
+            {
+                yield return new WaitForSeconds (dialogue.displayTime);
+            }
+            else
+            {
+                dialogueOptionsContainer.SetActive(true);
+                foreach (var option in dialogue.dialogueChoices)
+                {
+                    GameObject newButton = Instantiate(dialogueOptionsButtonPrefab, dialogueOptionsParent);
+                    instantiatedButtons.Add(newButton);
+                    newButton.GetComponent<UIDialogueOption>().Setup(this, option.followupDialogue, option.dialogueChoice);
+                }
+
+                while (!optionSelected)
+                {
+                    yield return null;
+                }
+                break;
+            }
         }
+
+        dialogueOptionsContainer.SetActive(false);
+        textBubble.SetActive(false);
+        interactUI.SetActive(false);
+        optionSelected = false;
+
+        instantiatedButtons.ForEach (x => Destroy (x));
+        Debug.Log("Ending Dialogue Chain");
     }
 }
--- Assets/JinsolTestDev/Scripts/Dialogue/DialogueSO.cs
+++ Assets/JinsolTestDev/Scripts/Dialogue/DialogueSO.cs
@@ -1,14 +1,28 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "DialogueSO", menuName = "ScriptableObjects/DialogueSO")]
+[CreateAssetMenu(fileName = "DialogueSO", menuName = "ScriptableObjects/NPC_Dialogue")]
 public class DialogueSO : ScriptableObject
 {
     [Header("현재 대화")]
-    public List<string> dialogueLines = new List<string>(); // 대사 목록. 외부에서 참조 가능하게
+    public List<DialogueSegment> dialogueSegments = new List<DialogueSegment>(); // 대사 목록. 외부에서 참조 가능하게
 
     [Header("추가 대화")]
     public DialogueSO endDialogue; // 대화 오브젝트 끝나면 다음 오브젝트로 넘겨줄 수 있게
+}
+
+[System.Serializable]
+public struct DialogueSegment
+{
+    public string dialogueText;
+    public float displayTime; // 대화 표시 시간 << 버튼으로 바꿀 것
+    public List<DialogueChoice> dialogueChoices;
+}
+
+[System.Serializable]
+public struct DialogueChoice
+{
+    public string dialogueChoice;
+    public DialogueSO followupDialogue;
 }
