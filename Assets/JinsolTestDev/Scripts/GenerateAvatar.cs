b8e94695952e8a63a1fb0d4f401ec2a7a3da361f
Jinsol Jung <jinsoljungjs@gmail.com>
2024-04-03 03:11:26 +0900

    Fix compiler error

--- Assets/JinsolTestDev/Scripts/GenerateAvatar.cs
+++ Assets/JinsolTestDev/Scripts/GenerateAvatar.cs
@@ -3,61 +3,59 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 
-    /// <summary>
-    /// 아바타가 없는 모델링의 아바타를 생성해 주는 스크립트입니다.
-    /// 생성을 원하는 모델링을 Hierarchy에서 선택 후
-    /// 상단 메뉴 CustomTools에서 원하는 아바타 종류를 선택하여 생성합니다.
-    /// 이 때 뼈 구조와 명칭은 이 스크립트에 정의된 것과 일치해야 합니다.
-    /// 
-    /// - 정진솔
-    /// </summary>
+/// <summary>
+/// 아바타가 없는 모델링의 아바타를 생성해 주는 스크립트입니다.
+/// 생성을 원하는 모델링을 Hierarchy에서 선택 후
+/// 상단 메뉴 CustomTools에서 원하는 아바타 종류를 선택하여 생성합니다.
+/// 이 때 뼈 구조와 명칭은 이 스크립트에 정의된 것과 일치해야 합니다.
+/// 
+/// - 정진솔
+/// </summary>
 
-namespace Jinsol
+namespace Infrastructure.Editor
 {
-    namespace Infrastructure.Editor
+    public class GenerateAvatar : MonoBehaviour
     {
-        public class GenerateAvatar : MonoBehaviour
+        [MenuItem("CustomTools/MakeAvatarMask")]
+        private static void MakeAvatarMask()
         {
-            [MenuItem("CustomTools/MakeAvatarMask")]
-            private static void MakeAvatarMask()
+            GameObject activeGameObject = Selection.activeGameObject;
+
+            if (activeGameObject != null)
             {
-                GameObject activeGameObject = Selection.activeGameObject;
+                AvatarMask avatarMask = new AvatarMask();
 
-                if (activeGameObject != null)
-                {
-                    AvatarMask avatarMask = new AvatarMask();
+                avatarMask.AddTransformPath(activeGameObject.transform);
 
-                    avatarMask.AddTransformPath(activeGameObject.transform);
-
-                    var path = string.Format("Assets/{0}.mask", activeGameObject.name.Replace(':', '_'));
-                    AssetDatabase.CreateAsset(avatarMask, path);
-                }
+                var path = string.Format("Assets/{0}.mask", activeGameObject.name.Replace(':', '_'));
+                AssetDatabase.CreateAsset(avatarMask, path);
             }
+        }
 
-            [MenuItem("CustomTools/MakeAvatar")]
-            private static void MakeAvatar()
+        [MenuItem("CustomTools/MakeAvatar")]
+        private static void MakeAvatar()
+        {
+            GameObject activeGameObject = Selection.activeGameObject;
+
+            if (activeGameObject != null)
             {
-                GameObject activeGameObject = Selection.activeGameObject;
+                Avatar avatar = AvatarBuilder.BuildGenericAvatar(activeGameObject, "");
+                avatar.name = activeGameObject.name;
+                Debug.Log(avatar.isHuman ? "is human" : "is generic");
 
-                if (activeGameObject != null)
-                {
-                    Avatar avatar = AvatarBuilder.BuildGenericAvatar(activeGameObject, "");
-                    avatar.name = activeGameObject.name;
-                    Debug.Log(avatar.isHuman ? "is human" : "is generic");
-
-                    var path = string.Format("Assets/{0}.ht", avatar.name.Replace(':', '_'));
-                    AssetDatabase.CreateAsset(avatar, path);
-                }
+                var path = string.Format("Assets/{0}.ht", avatar.name.Replace(':', '_'));
+                AssetDatabase.CreateAsset(avatar, path);
             }
+        }
 
-            [MenuItem("CustomTools/MakeBipedAvatar")]
-            private static void MakeBipedAvatar()
-            {
-                var activeGameObject = Selection.activeGameObject;
+        [MenuItem("CustomTools/MakeBipedAvatar")]
+        private static void MakeBipedAvatar()
+        {
+            var activeGameObject = Selection.activeGameObject;
 
-                if (activeGameObject == null) return;
+            if (activeGameObject == null) return;
 
-                var boneMapping = new Dictionary<string, string>
+            var boneMapping = new Dictionary<string, string>
                         {
                             {"Hips", "Bip001 Pelvis"},
                             {"LeftUpperLeg", "Bip001 L Thigh"},
@@ -83,66 +81,66 @@ namespace Jinsol
                             {"UpperChest", "Bip001 Spine2"},
                         };
 
-                var humanDescription = new HumanDescription
+            var humanDescription = new HumanDescription
+            {
+                human = boneMapping.Select(mapping =>
                 {
-                    human = boneMapping.Select(mapping =>
-                    {
-                        var bone = new HumanBone { humanName = mapping.Key, boneName = mapping.Value };
-                        bone.limit.useDefaultValues = true;
-                        return bone;
-                    }).ToArray(),
-                };
+                    var bone = new HumanBone { humanName = mapping.Key, boneName = mapping.Value };
+                    bone.limit.useDefaultValues = true;
+                    return bone;
+                }).ToArray(),
+            };
 
-                var avatar = AvatarBuilder.BuildHumanAvatar(activeGameObject, humanDescription);
-                avatar.name = activeGameObject.name;
+            var avatar = AvatarBuilder.BuildHumanAvatar(activeGameObject, humanDescription);
+            avatar.name = activeGameObject.name;
 
-                if (!avatar.isValid)
-                {
-                    Debug.LogError("Invalid avatar");
-                    return;
-                }
-
-                Debug.Log(avatar.isHuman ? "is human" : "is generic");
-
-                var path = $"Assets/{avatar.name.Replace(':', '_')}.ht";
-                AssetDatabase.CreateAsset(avatar, path);
-
+            if (!avatar.isValid)
+            {
+                Debug.LogError("Invalid avatar");
+                return;
             }
 
-            [MenuItem("CustomTools/MakeHumanAvatar")]
-            private static void MakeHumanAvatar()
-            {
-                var activeGameObject = Selection.activeGameObject;
+            Debug.Log(avatar.isHuman ? "is human" : "is generic");
 
-                if (activeGameObject == null) return;
+            var path = $"Assets/{avatar.name.Replace(':', '_')}.ht";
+            AssetDatabase.CreateAsset(avatar, path);
 
-                /*            var boneMapping = new Dictionary<string, string>
-                            {
-                                {"Hips", "DEF-spine"},
-                                {"LeftUpperLeg", "DEF-thigh.L"},
-                                {"RightUpperLeg", "DEF-thigh.R"},
-                                {"LeftLowerLeg", "DEF-shin.L"},
-                                {"RightLowerLeg", "DEF-shin.R"},
-                                {"LeftFoot", "DEF-foot.L"},
-                                {"RightFoot", "DEF-foot.R"},
-                                {"Spine", "DEF-spine.001"},
-                                {"Chest", "DEF-spine.002"},
-                                {"Neck", "DEF-neck"},
-                                {"Head", "DEF-head"},
-                                {"LeftShoulder", "DEF-shoulder.L"},
-                                {"RightShoulder", "DEF-shoulder.R"},
-                                {"LeftUpperArm", "DEF-upper_arm.L"},
-                                {"RightUpperArm", "DEF-upper_arm.R"},
-                                {"LeftLowerArm", "DEF-forearm.L"},
-                                {"RightLowerArm", "DEF-forearm.R"},
-                                {"LeftHand", "DEF-hand.L"},
-                                {"RightHand", "DEF-hand.R"},
-                                {"LeftToes", "DEF-toe.L"},
-                                {"RightToes", "DEF-toe.R"},
-                                {"UpperChest", "DEF-spine.003"},
-                            };*/
+        }
 
-                var boneMapping = new Dictionary<string, string>
+        [MenuItem("CustomTools/MakeHumanAvatar")]
+        private static void MakeHumanAvatar()
+        {
+            var activeGameObject = Selection.activeGameObject;
+
+            if (activeGameObject == null) return;
+
+            /*            var boneMapping = new Dictionary<string, string>
+                        {
+                            {"Hips", "DEF-spine"},
+                            {"LeftUpperLeg", "DEF-thigh.L"},
+                            {"RightUpperLeg", "DEF-thigh.R"},
+                            {"LeftLowerLeg", "DEF-shin.L"},
+                            {"RightLowerLeg", "DEF-shin.R"},
+                            {"LeftFoot", "DEF-foot.L"},
+                            {"RightFoot", "DEF-foot.R"},
+                            {"Spine", "DEF-spine.001"},
+                            {"Chest", "DEF-spine.002"},
+                            {"Neck", "DEF-neck"},
+                            {"Head", "DEF-head"},
+                            {"LeftShoulder", "DEF-shoulder.L"},
+                            {"RightShoulder", "DEF-shoulder.R"},
+                            {"LeftUpperArm", "DEF-upper_arm.L"},
+                            {"RightUpperArm", "DEF-upper_arm.R"},
+                            {"LeftLowerArm", "DEF-forearm.L"},
+                            {"RightLowerArm", "DEF-forearm.R"},
+                            {"LeftHand", "DEF-hand.L"},
+                            {"RightHand", "DEF-hand.R"},
+                            {"LeftToes", "DEF-toe.L"},
+                            {"RightToes", "DEF-toe.R"},
+                            {"UpperChest", "DEF-spine.003"},
+                        };*/
+
+            var boneMapping = new Dictionary<string, string>
             {
                 {"Hips", "torso_joint_1"},
                 {"LeftUpperLeg", "leg_joint_L_1"},
@@ -165,30 +163,29 @@ namespace Jinsol
                 {"RightToes", "leg_joint_R_5"},
             };
 
-                var humanDescription = new HumanDescription
+            var humanDescription = new HumanDescription
+            {
+                human = boneMapping.Select(mapping =>
                 {
-                    human = boneMapping.Select(mapping =>
-                    {
-                        var bone = new HumanBone { humanName = mapping.Key, boneName = mapping.Value };
-                        bone.limit.useDefaultValues = true;
-                        return bone;
-                    }).ToArray(),
-                };
+                    var bone = new HumanBone { humanName = mapping.Key, boneName = mapping.Value };
+                    bone.limit.useDefaultValues = true;
+                    return bone;
+                }).ToArray(),
+            };
 
-                var avatar = AvatarBuilder.BuildHumanAvatar(activeGameObject, humanDescription);
-                avatar.name = activeGameObject.name;
+            var avatar = AvatarBuilder.BuildHumanAvatar(activeGameObject, humanDescription);
+            avatar.name = activeGameObject.name;
 
-                if (!avatar.isValid)
-                {
-                    Debug.LogError("Invalid avatar");
-                    return;
-                }
-
-                Debug.Log(avatar.isHuman ? "is human" : "is generic");
-
-                var path = $"Assets/{avatar.name.Replace(':', '_')}.ht";
-                AssetDatabase.CreateAsset(avatar, path);
+            if (!avatar.isValid)
+            {
+                Debug.LogError("Invalid avatar");
+                return;
             }
+
+            Debug.Log(avatar.isHuman ? "is human" : "is generic");
+
+            var path = $"Assets/{avatar.name.Replace(':', '_')}.ht";
+            AssetDatabase.CreateAsset(avatar, path);
         }
     }
 }
