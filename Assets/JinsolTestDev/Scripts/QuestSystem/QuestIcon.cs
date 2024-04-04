bc0ca27590e15f884b55a0f5e4f2a47643a15434
Jinsol Jung <jinsolfernanda@gmail.com>
2024-04-04 10:31:55 +0900

    퀘스트에 종속된 서브퀘스트들의 아이콘의 색상이 부모 퀘스트의 상태에 따라 같이 변하는 현상 수정

--- Assets/JinsolTestDev/Scripts/QuestSystem/QuestIcon.cs
+++ Assets/JinsolTestDev/Scripts/QuestSystem/QuestIcon.cs
@@ -14,6 +14,10 @@ namespace Jinsol
         [Header("Icons")]
         [SerializeField] private GameObject questIcon;
         [SerializeField] private Material material;
+
+        // 현재 퀘스트포인트 하위의 퀘스트포인트 아이콘의 색깔이 진행 상황에 맞춰 덩달아 변하지 않게 하는 부분
+        public bool isSubQuest = false; // 기본값은 false. 값 설정은 하위 퀘스트 포인트에 첨부되는 (e.g. Token.cs) 스크립트에서 실행
+
         /*    [SerializeField] private GameObject canStartIcon;
             [SerializeField] private GameObject cannotCompleteIcon;
             [SerializeField] private GameObject canCompleteIcon;*/
@@ -67,35 +71,40 @@ namespace Jinsol
             #endregion
 
             #region 퀘스트 상태에 맞춰서 아이콘 상태/색상 변화
-            switch (newState)
+            if (isSubQuest)
+                return;
+            else
             {
-                case QuestState.REQUIREMENTS_NOT_MET:
-                    break;
-                case QuestState.CAN_START:
-                    if (startPoint)
-                    {
-                        questIcon.SetActive(true);
-                    }
-                    break;
-                case QuestState.IN_PROGRESS:
-                    if (startPoint)
-                    {
-                        material.color = new Color(204f / 255f, 0f, 28f / 255f, 1f); // 퀘스트 진행중일 때 붉게 표시
-                        questIcon.SetActive(true);
-                    }
-                    break;
-                case QuestState.CAN_COMPLETE:
-                    if (startPoint)
-                    {
-                        material.color = new Color(0f, 168f / 255f, 102f / 255f, 1f); // 퀘스트 완료 가능할 때 녹색으로 표시
-                        questIcon.SetActive(true);
-                    }
-                    break;
-                case QuestState.COMPLETED:
-                    break;
-                default:
-                    Debug.LogWarning("퀘스트 아이콘을 찾을 수 없습니다: " + newState);
-                    break;
+                switch (newState)
+                {
+                    case QuestState.REQUIREMENTS_NOT_MET:
+                        break;
+                    case QuestState.CAN_START:
+                        if (startPoint)
+                        {
+                            questIcon.SetActive(true);
+                        }
+                        break;
+                    case QuestState.IN_PROGRESS:
+                        if (startPoint)
+                        {
+                            material.color = new Color(204f / 255f, 0f, 28f / 255f, 1f); // 퀘스트 진행중일 때 붉게 표시
+                            questIcon.SetActive(true);
+                        }
+                        break;
+                    case QuestState.CAN_COMPLETE:
+                        if (startPoint)
+                        {
+                            material.color = new Color(0f, 168f / 255f, 102f / 255f, 1f); // 퀘스트 완료 가능할 때 녹색으로 표시
+                            questIcon.SetActive(true);
+                        }
+                        break;
+                    case QuestState.COMPLETED:
+                        break;
+                    default:
+                        Debug.LogWarning("퀘스트 아이콘을 찾을 수 없습니다: " + newState);
+                        break;
+                }
             }
             #endregion
         }
